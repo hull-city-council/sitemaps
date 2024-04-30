c98ffe5342f122a847282455bf88d4f75e4332bd
HCCDanny <117913283+HCCDanny@users.noreply.github.com>
2024-04-30 09:45:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async function fetchURL(url) {
   } catch (error) {
     if (error.response) {
       console.error(`Response error fetching ${url}: ${error}`);
-      return null;
+      return;
     }
     console.error(`Error fetching ${url}: ${error}`);
     return null;
